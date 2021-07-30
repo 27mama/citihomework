b2a0b8da0cf2567ad656ca255c18121bc607ed0f
Gogs <gogs@fake.local>
2021-07-30 17:03:57 +0800

    第一次提交

---
+++ a00-hello/3/com/Hello2.java
@@ -0,0 +1,6 @@
+package com;
+public class Hello2 {
+	public static void main(String[] args) {
+		System.out.println("hello java2!");
+	}
+}
---
+++ a00-hello/4/Hello.java
@@ -0,0 +1,7 @@
+import com.hello2
+public class Hello {
+	 Hello2  hello2=new Hello2();
+	public static void main(String[] args) {
+		System.out.println("hello java!");
+	}
+}
