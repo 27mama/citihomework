c8eb82e35481a2ba11e878b83fea8edc90e128cf
Gogs <gogs@fake.local>
2021-07-30 17:04:55 +0800

    第一次提交

--- Run.java
+++
@@ -1,8 +0,0 @@
-//最终程序
-public class Run {
-	public static void main(String[] args) {
-		System.out.println(Convert2.toUpper2("hello java!"));
-		String  stringObject =new String("hello java!");
-		System.out.println(stringObject);
-	}
-}
